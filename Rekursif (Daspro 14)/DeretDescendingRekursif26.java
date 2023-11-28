b49b76938904fdaebf03c5e32b451646984d8131
SatriaNadirakhma <144087759+SatriaNadirakhma@users.noreply.github.com>
2023-11-28 15:00:20 +0700

    Praktikum Daspro 14_Satria Rakhmadani_26

---
+++ Rekursif (Daspro 14)/DeretDescendingRekursif26.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class DeretDescendingRekursif26 {
+    public static void tampilBilanganRekursif(int n) {
+        if (n > 0) {
+            System.out.println(n);
+            tampilBilanganRekursif(n - 1);
+        } else {
+            System.out.println(n);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan nilai n: ");
+        int n = sc.nextInt();
+        tampilBilanganRekursif(n);
+    }
+}
